6621f74ce99cb2abbf7d1a427d5aad20bb1d350b
Angelika Blackburn <bma89contact@gmail.com>
2020-09-18 12:10:22 -0400

    console working, fixed errors in code, prototypes done

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -30,7 +30,7 @@ CuboidMaker.prototype.volume = function() {
 */
 
 CuboidMaker.prototype.surfaceArea = function() {
-  return 2 * (length * width + length * height + width * height);
+  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
 };
 
 
